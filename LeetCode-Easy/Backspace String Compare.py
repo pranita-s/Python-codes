0471a3a6894cd27c7e7167efb706ceb46c88b7a5
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-04 18:38:11 +0530

    Easy

---
+++ LeetCode-Easy/Backspace String Compare.py
@@ -0,0 +1,24 @@
+ef backspaceCompare(S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        s1,s2 = [], []
+        
+        for char in S:
+            if s1 and char == '#':
+                s1.pop()
+            elif char != '#':
+                s1.append(char)
+        
+        for char in T:
+            if s2 and char == '#':
+                s2.pop()
+            elif char != '#':
+                s2.append(char)
+        print(s1,s2)
+        if not s1 and not s2:
+            return True
+        else:
+            return s1==s2
