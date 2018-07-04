77c2acd74b0e66ad0c879b860ce7b1d681436413
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-04 16:06:07 +0530

    Easy

--- LeetCode-Easy/Rotate String.py
+++ LeetCode-Easy/Rotate String.py
@@ -1,4 +1,47 @@
-# TIME - O(n)
+# TIME - O(m + n)
+# SPACE - O(n)
+
+def createLookupTable(s):
+  lookup = [0] * len(s)
+  j = 0
+  for i in range(1,len(s)):
+    if s[j] == s[i]:
+      lookup[i] = j + 1
+      j += 1
+      i += 1
+    else:
+      if j == 0:
+        i += 1
+      else:
+        j = lookup[j-1]
+ 
+ return lookup
+      
+
+def KMP(str,substring):
+  lookup = createLookupTable(substring)
+  i , j = 0, 0
+  while i < len(str) and j < len(substring):
+    if s[i] == s[j]:
+      i += 1
+      j += 1
+    else:
+      if j == 0:
+        i += 1
+      else:
+        j = lookup[j-1]
+    if j == len(substring):
+      return True
+  
+  return False
+
+def rotateString(s,t):
+  if len(s) != len(t):
+    return False
+  
+  return KMP(s+s,t)
+
+############################################################################
 
 def rotateString(A, B):
         """
