ab86e407bea1bb538acbba8f8676a41c4694fc88
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-07 12:49:32 +0530

    Easy

--- LeetCode-Easy/Position of Large Groups.py
+++ LeetCode-Easy/Position of Large Groups.py
@@ -5,7 +5,7 @@ def largeGroupPositions(S):
     i = 0
     pos = []
     for j in range(1,len(S)):
-      if j > 0 and S[j] != S[j+1]:
+      if j == len(S)-1 or S[j] != S[j+1]:
         if j-1+1 >= 3:
           pos.append([i,j])
         i = j + 1
