3bfef0367f3a492befb9f03a4225200d4d670686
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-07 12:38:28 +0530

    Easy

---
+++ LeetCode-Easy/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,12 @@
+# TIME - O(n)
+
+def maxProfit(prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i-1] < prices[i]:
+                profit += prices[i]-prices[i-1]
+        return profit
