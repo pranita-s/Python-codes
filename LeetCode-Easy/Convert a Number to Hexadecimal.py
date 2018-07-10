cf275f2ba8c1cc58a67280c9833b87ca44d58dde
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-10 11:44:33 +0530

    Easy

---
+++ LeetCode-Easy/Convert a Number to Hexadecimal.py
@@ -0,0 +1,14 @@
+# TIME - O(logn) base = 16
+
+
+def hexToDecimal(n):
+  d =  {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'a',11:'b',12:'c',13:'d',14:'e',15:'f'}
+  res = ''
+  if num < 0:
+    num = (-num ^ 0xffffffff)+1
+  elif num == 0:
+    return '0'
+  while n:
+    res = d[(n%16)= + res
+    n /= 16
+  return  n
