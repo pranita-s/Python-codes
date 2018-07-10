cb09085b6c21e374177c446d9e4deaee86b8520b
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-10 12:20:15 +0530

    Easy

---
+++ LeetCode-Easy/Paint House.py
@@ -0,0 +1,12 @@
+# TIME - O(n)
+
+
+def paintHouse(costs):
+  n = len(costs)
+  
+  for i in range(1,n):
+    costs[i][0] += min(costs[i-1][1], costs[i-1][2])
+    costs[i][1] += min(costs[i-1][2], costs[i-1][0])
+    costs[i][2] += min(costs[i-1][1], costs[i-1][0])
+  
+  return min(costs[n-1])
