f7f451f2fd71ab1e365302bb811239079e188606
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-18 08:24:59 +0530

    Easy

--- LeetCode-Easy/Power of Three.py
+++ LeetCode-Easy/Power of Three.py
@@ -1,10 +1,15 @@
 # TIME - O(logn)
 
-def powerOfThree(n):
-  
+def powerOfThree(n):  
   if n == 0:
     return False
   while n % 3 == 0:
-    n /= 3
-  
+    n /= 3  
   return n == 1
+
+
+# TIME - O(1)
+
+import math
+def powerofThree2(n):
+    return n > 0 and (math.log10(n)/math.log10(3)).is_integer()
