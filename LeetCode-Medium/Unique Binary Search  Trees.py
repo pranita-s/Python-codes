b1901a8092cd731c74d623f4e506cfa26fb4a121
Pranita Sharma <pranitarsharma@gmail.com>
2018-07-23 12:14:10 +0530

    Medium

---
+++ LeetCode-Medium/Unique Binary Search  Trees.py
@@ -0,0 +1,14 @@
+# TIME - O(n)
+
+
+def uniqueBSTtrees(n):
+
+  dp = [0]*(n+1)
+  dp[0] = 1
+  dp[1] = 1
+  
+  for i in range(2,n+1):
+    for j in range(1,n+1):
+      dp[i] += dp[i-1] * dp[i-j]
+  
+  return dp[n]
